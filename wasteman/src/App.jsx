00d50e6b312daec42e7f4fad42230744392a9a8d
patraarka <113913573+patraarka@users.noreply.github.com>
2025-12-13 16:23:47 +0000

    Modified the UI

--- wasteman/src/App.jsx
+++ wasteman/src/App.jsx
@@ -9,8 +9,15 @@ function App() {
       <div style={{
         backgroundImage: "url(src/assets/img2.png)"
       }} className='h-125 w-full bg-cover bg-center'>
-        <div className='bg-gray-700 opacity-50 h-full w-full'>
-
+        <div className='bg-black/50 h-full w-full'>
+          <nav className='flex p-5'>
+            <div><img alt="logo" src="src/assets/logo.png" h={100} w={100} className="h-15 w-15"/>
+            </div>
+            <div>
+              <p className='text-3xl text-blue-500 font-bold py-0 my-0'>WASTE</p>
+              <p className='text-xl text-blue-500 py-0 my-0'>Man</p>
+            </div>
+          </nav>
         </div>
       </div>
     </>

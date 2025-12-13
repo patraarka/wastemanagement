70258977ae08f52500aae5be8573db63b15e93ca
patraarka <113913573+patraarka@users.noreply.github.com>
2025-12-13 11:55:20 +0000

    Made Changes

--- wasteman/src/App.jsx
+++ wasteman/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
 import './App.css'
-import logo from "./assets/image.png";
 
 function App() {
   const [count, setCount] = useState(0)
+
   return (
     <>
-      <div className=''>         
+      <div style={{
+        backgroundImage: "url(src/assets/img2.png)"
+      }} className='h-125 w-full bg-cover bg-center'>
+        <div className='bg-gray-700 opacity-50 h-full w-full'>
+
+        </div>
       </div>
-      <p>Arka</p>
     </>
   )
 }

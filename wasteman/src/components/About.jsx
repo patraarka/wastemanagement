35e8acb39f1d086ebf0411315b221b0a24721383
patraarka <113913573+patraarka@users.noreply.github.com>
2025-12-13 21:57:45 +0000

    Added the About Section

--- wasteman/src/components/About.jsx
+++ wasteman/src/components/About.jsx
@@ -3,7 +3,15 @@ import React from "react";
 const About=()=>{
     return(
         <>
-
+            <div style={{
+                backgroundImage: "url(src/assets/img1.png)"
+                }} className='h-125 w-full bg-cover bg-center'>
+                <div className="bg-white/50 w-full h-full">
+                <div>
+                    
+                </div>
+                </div>
+            </div>
         </>
     );
 };

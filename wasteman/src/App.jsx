4b51264d8032471cdbfeae3226e73bceac779446
patraarka <113913573+patraarka@users.noreply.github.com>
2025-12-13 11:03:06 +0000

    Created the files for the project.

---
+++ wasteman/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import logo from "./assets/image.png";
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <>
+      <div className=''>         
+      </div>
+      <p>Arka</p>
+    </>
+  )
+}
+
+export default App
